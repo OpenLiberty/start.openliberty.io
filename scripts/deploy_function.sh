0d277ae0afdc4b99aca7fd6f11c158b0ea980ed3
steven1046 <stevenzvonek1992@gmail.com>
2021-04-14 13:53:14 -0500

    Add deploy scripts

---
+++ scripts/deploy_function.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function pushApp {
+  cf push "${CF_APP}" --no-start
+  if [[ -z $CURRENT_STATE ]]
+  then
+    cf bind-service "${CF_APP}" "${AVAILABILITY_SERVICE}"
+  fi
+  cf start "${CF_APP}"
+  cf set-env "${CF_APP}" LAST_DEPLOY_NUMBER "${BUILD_NUMBER}"
+}
