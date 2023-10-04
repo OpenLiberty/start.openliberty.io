9b36e624a75f756d972446aeedb73a7006d28100
SteveSamJacob19 <Steve.Sam.Jacob1@ibm.com>
2023-10-04 18:53:55 +0530

    Made some edits

--- src/test/java/io/openliberty/website/starter/GradleStarterBuilderTest.java
+++ src/test/java/io/openliberty/website/starter/GradleStarterBuilderTest.java
@@ -23,7 +23,7 @@ public class GradleStarterBuilderTest {
     @Test
     public void testJakartaEE8() throws UnsupportedEncodingException, IOException, XmlPullParserException {
         MockZipOutputStream mockZip = MockZipOutputStream.create().capture("build.gradle").capture("src/main/liberty/config/server.xml").capture("settings.gradle");
-        BuildSystemType.gradle.create().appName("test").groupName("io.openliberty.demo").javaVersion("7")
+        BuildSystemType.gradle.create().appName("test").groupName("io.openliberty.demo").javaVersion("17")
                 .jakartaEEVersion("8.0").microProfileVersion("3.3").buildType("gradle").build(mockZip);
 
         assertFilesPresent(mockZip);
